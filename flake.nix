fb92341a2e69aa1161b9486ae728cd1382735055
Kenneth Hoff <kenneth.98@hotmail.com>
2025-06-21 22:41:14 +0200

    Add HomeModule

--- flake.nix
+++ flake.nix
@@ -5,58 +5,71 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nvf.url = "github:notashelf/nvf";
     flake-utils.url = "github:numtide/flake-utils";
+    home-manager.url = "github:nix-community/home-manager"; # Added home-manager input
   };
 
-  outputs =
-    {
-      flake-utils,
-      nvf,
-      nixpkgs,
-      ...
-    }:
-    let
-      # Define the module outside eachDefaultSystem so it's available at the top level
-      nvf-config-module =
-        {
-          pkgs,
-          lib,
-          config,
-          ...
-        }:
-        {
-          options.programs.nvf-config = {
-            enable = lib.mkEnableOption "Custom Neovim configuration via nvf";
-          };
-
-          config = lib.mkIf config.programs.nvf-config.enable {
-            environment.systemPackages = [
-              (nvf.lib.neovimConfiguration {
-                inherit pkgs;
-                modules = [
-                  (import ./nvf.nix { })
-                ];
-              }).neovim
-            ];
-          };
-        };
-    in
-    (flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages.default =
-          (nvf.lib.neovimConfiguration {
+  outputs = {
+    flake-utils,
+    nvf,
+    nixpkgs,
+    home-manager,
+    ...
+  }: let
+    mkConfig = pkgs:
+      (nvf.lib.neovimConfiguration {
+        inherit pkgs;
+        modules = [
+          (import ./nvf.nix {
             inherit pkgs;
-            modules = [
-              (import ./nvf.nix { })
-            ];
-          }).neovim;
+          })
+        ];
+      }).neovim;
+    # Define the module outside eachDefaultSystem so it's available at the top level
+    nvf-config-module = {
+      pkgs,
+      lib,
+      config,
+      ...
+    }: {
+      options.programs.nvf-config = {
+        enable = lib.mkEnableOption "Custom Neovim configuration via nvf";
+      };
+
+      config = lib.mkIf config.programs.nvf-config.enable {
+        environment.systemPackages = [
+          (mkConfig pkgs)
+        ];
+      };
+    };
+
+    # Home Manager module
+    hm-nvf-config-module = {
+      pkgs,
+      lib,
+      config,
+      ...
+    }: {
+      options.programs.nvf-config = {
+        enable = lib.mkEnableOption "Custom Neovim configuration via nvf";
+      };
+
+      config = lib.mkIf config.programs.nvf-config.enable {
+        home.packages = [
+          (mkConfig pkgs)
+        ];
+      };
+    };
+  in
+    (flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = mkConfig pkgs;
       }
     ))
     // {
-      # Expose the module at the top level with the name your main flake is looking for
+      # Expose both modules at the top level
       nixosModules.nvf-config = nvf-config-module;
+      homeModules.nvf-config = hm-nvf-config-module;
     };
 }
